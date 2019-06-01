284dfd56620da498cd8b2036f745750d8badf6e1
rohit236c <rohit236c@gmail.com>
2019-06-01 23:02:51 +0530

    determing temp using api
    Signed-off-by: rohit236c <rohit236c@gmail.com>

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const request = require('request');
 
 const url = 'https://api.darksky.net/forecast/5ade034ffc85861ad8d8b4234d07f9aa/37.8267,-122.4233';
 
-request({url : url},(err,response)=>{
+request({url : url, json : true},(err,response)=>{
     // console.log(response);
-    const data = JSON.parse(response.body);
-    console.log(data.currently)
+   console.log('It is ' + response.body.currently.temperature + ' degress out there');
 })
